3a4c1f8201b3259cf9cb89785741e9e9818e508c
knaseem <knaseem22@gmail.com>
2016-06-30 19:05:52 -0400

    Added buttons and labels on main view

--- SwiftQuiz/ViewController.swift
+++ SwiftQuiz/ViewController.swift
@@ -9,17 +9,17 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    
+    @IBAction func showAnswer(sender: UIButton) {
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @IBAction func nextQuestion(sender: UIButton) {
     }
+    
+    
+    @IBOutlet weak var answerLabel: UILabel!
+    @IBOutlet weak var questionLabel: UILabel!
 
-
+    
 }
 
